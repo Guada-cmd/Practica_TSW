ef9841bd3e6c1ae7f045a95f7ac7bdb863476f67
Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
2021-11-12 12:50:09 +0100

    Segundo commit

---
+++ PracticaTSW/src/test/java/com/example/PracticaTSW/PracticaTswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticaTSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaTswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
